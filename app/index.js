c584b0543b9761884f4360ebcbb992874384ea10
senorjah <jahsomeyt@gmail.com>
2025-02-07 16:32:38 -0800

    init-commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import page from "./page";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <Page />
+    </React.StrictMode>
+);
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Page() {
+  return (
+    <div>
+      <h1>Wlecome to calvent</h1>
+      <p>This is the main page of my app.</p>
+    </div>
+  );
+}
+
+export default Page;
